b9669a2447b9fcb4970bb0eb46b50f30e1322893
Loknath Dhar <loknath-dhar@pm.me>
2025-01-01 23:33:11 +0600

    adding get and post req for med

--- main.go
+++ main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"medicine-app/internal/database"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env file: %v\n", err)
+	}
+
+	dbURL := getEnvVariable("DB_URL")
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("can't connect to database: %v\n", err)
+	}
+	dbQueries := database.New(dbConn)
+
 	r := gin.Default()
 
 	medicineApp := MedicineApp{
+		DB:     dbQueries,
 		Router: r,
 	}
 
-	r.POST("/medicine", medicineApp.CreateMedicine)
+	r.POST("/medicines", medicineApp.CreateMedicine)
+	r.GET("/medicines", medicineApp.GetMedicine)
 
 	r.Run(":8080")
 
@@ -24,13 +43,22 @@ func main() {
 
 }
 
+func getEnvVariable(env string) string {
+	envVar := os.Getenv(env)
+	if envVar == "" {
+		log.Fatalf("%s must be set", env)
+	}
+
+	return envVar
+}
+
 type Medicine struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Dosage       string `json:"dosage"`
-	Manufacturer string `json:"manufacturer"`
-	Price        int    `json:"price"`
-	Stock        int    `json:"stock"`
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Dosage       string    `json:"dosage"`
+	Manufacturer string    `json:"manufacturer"`
+	Price        int       `json:"price"`
+	Stock        int       `json:"stock"`
 	Created_at   time.Time
 	Updated_at   time.Time
 }
@@ -48,7 +76,7 @@ func (medApp MedicineApp) CreateMedicine(ctx *gin.Context) {
 		return
 	}
 
-	if err := medApp.DB.CreateMedicine(ctx.Request.Context(), database.CreateMedicineParams{
+	if err := medApp.DB.CreateMedicine(ctx, database.CreateMedicineParams{
 		Name:         newMedicine.Name,
 		Dosage:       newMedicine.Dosage,
 		Manufacturer: newMedicine.Manufacturer,
@@ -61,5 +89,17 @@ func (medApp MedicineApp) CreateMedicine(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, newMedicine)
 }
 
-// func (medApp MedicineApp) GetMedicine(ctx *gin.Context) {
-// }
+func (medApp MedicineApp) GetMedicine(ctx *gin.Context) {
+
+	medicines, err := medApp.DB.GetMedicines(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorPass(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, medicines)
+}
+
+func errorPass(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
