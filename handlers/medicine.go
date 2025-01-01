b53ff094dba4d8699564f22c10674074a23e3466
Loknath Dhar <loknath-dhar@pm.me>
2025-01-01 23:36:52 +0600

    separate handler

--- handlers/medicine.go
+++ handlers/medicine.go
@@ -1,28 +1,62 @@
 package handlers
 
-// type Medicine struct {
-// 	ID           int     `json:"id"`
-// 	Name         string  `json:"name"`
-// 	Dosage       string  `json:"dosage"`
-// 	Manufacturer string  `json:"manufacturer"`
-// 	Price        float64 `json:"price"`
-// 	Stock        int     `json:"stock"`
-// 	Created_at   time.Time
-// 	Updated_at   time.Time
-// }
+import (
+	"medicine-app/internal/database"
+	"net/http"
+	"time"
 
-// type MedicineApp struct {
-// 	Router *gin.Engine
-// }
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
-// var medicines = []Medicine{}
+type Medicine struct {
+	ID           uuid.UUID `json:"id"`
+	Name         string    `json:"name"`
+	Dosage       string    `json:"dosage"`
+	Manufacturer string    `json:"manufacturer"`
+	Price        int       `json:"price"`
+	Stock        int       `json:"stock"`
+	Created_at   time.Time
+	Updated_at   time.Time
+}
 
-// func (medApp MedicineApp) CreateMedicine(ctx *gin.Context) {
-// 	var newMedicine Medicine
-// 	if err := ctx.ShouldBindJSON(&newMedicine); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	medicines = append(medicines, newMedicine)
-// 	ctx.JSON(http.StatusOK, newMedicine)
-// }
+type MedicineApp struct {
+	DB     *database.Queries
+	Router *gin.Engine
+}
+
+func (medApp MedicineApp) CreateMedicine(ctx *gin.Context) {
+	var newMedicine Medicine
+
+	if err := ctx.ShouldBindJSON(&newMedicine); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := medApp.DB.CreateMedicine(ctx, database.CreateMedicineParams{
+		Name:         newMedicine.Name,
+		Dosage:       newMedicine.Dosage,
+		Manufacturer: newMedicine.Manufacturer,
+		Price:        int32(newMedicine.Price),
+		Stock:        int32(newMedicine.Stock),
+	}); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	ctx.JSON(http.StatusOK, newMedicine)
+}
+
+func (medApp MedicineApp) GetMedicine(ctx *gin.Context) {
+
+	medicines, err := medApp.DB.GetMedicines(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorPass(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, medicines)
+}
+
+func errorPass(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"database/sql"
 	"log"
+	"medicine-app/handlers"
 	"medicine-app/internal/database"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -29,17 +27,15 @@ func main() {
 
 	r := gin.Default()
 
-	medicineApp := MedicineApp{
+	medApp := handlers.MedicineApp{
 		DB:     dbQueries,
 		Router: r,
 	}
 
-	r.POST("/medicines", medicineApp.CreateMedicine)
-	r.GET("/medicines", medicineApp.GetMedicine)
+	r.POST("/medicines", medApp.CreateMedicine)
+	r.GET("/medicines", medApp.GetMedicine)
 
 	r.Run(":8080")
-
-	// listen and serve on :8080
 
 }
 
@@ -50,56 +46,4 @@ func getEnvVariable(env string) string {
 	}
 
 	return envVar
-}
-
-type Medicine struct {
-	ID           uuid.UUID `json:"id"`
-	Name         string    `json:"name"`
-	Dosage       string    `json:"dosage"`
-	Manufacturer string    `json:"manufacturer"`
-	Price        int       `json:"price"`
-	Stock        int       `json:"stock"`
-	Created_at   time.Time
-	Updated_at   time.Time
-}
-
-type MedicineApp struct {
-	DB     *database.Queries
-	Router *gin.Engine
-}
-
-func (medApp MedicineApp) CreateMedicine(ctx *gin.Context) {
-	var newMedicine Medicine
-
-	if err := ctx.ShouldBindJSON(&newMedicine); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if err := medApp.DB.CreateMedicine(ctx, database.CreateMedicineParams{
-		Name:         newMedicine.Name,
-		Dosage:       newMedicine.Dosage,
-		Manufacturer: newMedicine.Manufacturer,
-		Price:        int32(newMedicine.Price),
-		Stock:        int32(newMedicine.Stock),
-	}); err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	}
-
-	ctx.JSON(http.StatusOK, newMedicine)
-}
-
-func (medApp MedicineApp) GetMedicine(ctx *gin.Context) {
-
-	medicines, err := medApp.DB.GetMedicines(ctx)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorPass(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, medicines)
-}
-
-func errorPass(err error) gin.H {
-	return gin.H{"error": err.Error()}
 }
