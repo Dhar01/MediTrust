eb2cb86697d600f2d5c11bb05ff4eac86400cf42
Loknath Dhar <loknath-dhar@pm.me>
2025-01-06 18:10:43 +0600

    separating service and controllers

--- controllers/medicine.go
+++ controllers/medicine.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"net/http"
 
-	"medicine-app/internal/database"
 	"medicine-app/models"
 
 	"github.com/gin-gonic/gin"
@@ -11,35 +10,24 @@ import (
 )
 
 type medicineController struct {
-	DB *database.Queries
+	MedicineService models.MedicineService
 }
 
-func NewMedicineController(db *database.Queries) *medicineController {
-	if db == nil {
-		panic("database can't be nil")
-	}
-
+func NewMedicineController(service models.MedicineService) *medicineController {
 	return &medicineController{
-		DB: db,
+		MedicineService: service,
 	}
 }
 
-func (mc *medicineController) CreateMedicineHandler(ctx *gin.Context) {
-	var newMedicine models.MedicineBody
+func (mc *medicineController) HandlerCreateMedicineHandler(ctx *gin.Context) {
+	var newMedicine models.Medicine
 
 	if err := ctx.ShouldBindJSON(&newMedicine); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorMsg(err))
 		return
 	}
 
-	medicine, err := mc.DB.CreateMedicine(ctx, database.CreateMedicineParams{
-		Name:         newMedicine.Name,
-		Dosage:       newMedicine.Dosage,
-		Manufacturer: newMedicine.Manufacturer,
-		Price:        newMedicine.Price,
-		Stock:        newMedicine.Stock,
-	})
-
+	medicine, err := mc.MedicineService.CreateMedicine(ctx, newMedicine)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
 		return
@@ -48,8 +36,8 @@ func (mc *medicineController) CreateMedicineHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, medicine)
 }
 
-func (mc *medicineController) GetMedicines(ctx *gin.Context) {
-	medicines, err := mc.DB.GetMedicines(ctx)
+func (mc *medicineController) HandlerGetMedicines(ctx *gin.Context) {
+	medicines, err := mc.MedicineService.GetMedicines(ctx)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
 		return
@@ -58,13 +46,13 @@ func (mc *medicineController) GetMedicines(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, medicines)
 }
 
-func (mc *medicineController) GetMedicineByID(ctx *gin.Context) {
+func (mc *medicineController) HandlerGetMedicineByID(ctx *gin.Context) {
 	id, ok := getMedicineID(ctx)
 	if !ok {
 		return
 	}
 
-	medicine, err := mc.DB.GetMedicine(ctx, id)
+	medicine, err := mc.MedicineService.GetMedicineByID(ctx, id)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
 		return
@@ -73,29 +61,20 @@ func (mc *medicineController) GetMedicineByID(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, medicine)
 }
 
-func (mc *medicineController) UpdateMedicine(ctx *gin.Context) {
+func (mc *medicineController) HandlerUpdateMedicine(ctx *gin.Context) {
 	id, ok := getMedicineID(ctx)
 	if !ok {
 		return
 	}
 
-	var updateMed models.MedicineBody
+	var updateMed models.Medicine
 
 	if err := ctx.ShouldBindJSON(&updateMed); err != nil {
 		ctx.IndentedJSON(http.StatusBadRequest, errorMsg(err))
 		return
 	}
 
-	medicine, err := mc.DB.UpdateMedicine(ctx, database.UpdateMedicineParams{
-		ID:           id,
-		Name:         updateMed.Name,
-		Dosage:       updateMed.Dosage,
-		Manufacturer: updateMed.Manufacturer,
-		Description:  updateMed.Description,
-		Price:        updateMed.Price,
-		Stock:        updateMed.Stock,
-	})
-
+	medicine, err := mc.MedicineService.UpdateMedicine(ctx, id, updateMed)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
 		return
@@ -104,13 +83,13 @@ func (mc *medicineController) UpdateMedicine(ctx *gin.Context) {
 	ctx.JSON(http.StatusAccepted, medicine)
 }
 
-func (mc *medicineController) DeleteMedicine(ctx *gin.Context) {
+func (mc *medicineController) HandlerDeleteMedicine(ctx *gin.Context) {
 	id, ok := getMedicineID(ctx)
 	if !ok {
 		return
 	}
 
-	if err := mc.DB.DeleteMedicine(ctx, id); err != nil {
+	if err := mc.MedicineService.DeleteMedicine(ctx, id); err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
 		return
 	}
---
+++ services/medicine.go
@@ -0,0 +1,136 @@
+package service
+
+import (
+	"context"
+	"medicine-app/internal/database"
+	"medicine-app/models"
+
+	"github.com/google/uuid"
+)
+
+type medicineService struct {
+	DB *database.Queries
+}
+
+func NewMedicineService(db *database.Queries) models.MedicineService {
+	if db == nil {
+		panic("database can't be nil")
+	}
+
+	return &medicineService{
+		DB: db,
+	}
+}
+
+func (ms *medicineService) CreateMedicine(ctx context.Context, newMed models.Medicine) (models.Medicine, error) {
+	medicine, err := ms.DB.CreateMedicine(ctx, database.CreateMedicineParams{
+		Name:         newMed.Name,
+		Dosage:       newMed.Dosage,
+		Description:  newMed.Description,
+		Manufacturer: newMed.Manufacturer,
+		Price:        newMed.Price,
+		Stock:        newMed.Stock,
+	})
+
+	if err != nil {
+		return models.Medicine{}, err
+	}
+
+	// return models.Medicine{
+	// 	ID:           medicine.ID,
+	// 	Name:         medicine.Name,
+	// 	Description:  medicine.Description,
+	// 	Manufacturer: medicine.Manufacturer,
+	// 	Dosage:       medicine.Dosage,
+	// 	Price:        medicine.Price,
+	// 	Stock:        medicine.Stock,
+	// }, nil
+
+	return toMedicineDomain(medicine), nil
+}
+
+func (ms *medicineService) DeleteMedicine(ctx context.Context, medID uuid.UUID) error {
+	if err := ms.DB.DeleteMedicine(ctx, medID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ms *medicineService) GetMedicines(ctx context.Context) ([]models.Medicine, error) {
+	var medList []models.Medicine
+
+	medicines, err := ms.DB.GetMedicines(ctx)
+	if err != nil {
+		return medList, err
+	}
+
+	for _, medicine := range medicines {
+		medList = append(medList, toMedicineDomain(medicine))
+	}
+
+	return medList, nil
+}
+
+func (ms *medicineService) GetMedicineByID(ctx context.Context, medID uuid.UUID) (models.Medicine, error) {
+	medicine, err := ms.DB.GetMedicine(ctx, medID)
+	if err != nil {
+		return models.Medicine{}, err
+	}
+
+	// return models.Medicine{
+	// 	ID:           medicine.ID,
+	// 	Name:         medicine.Name,
+	// 	Description:  medicine.Description,
+	// 	Manufacturer: medicine.Manufacturer,
+	// 	Dosage:       medicine.Dosage,
+	// 	Price:        medicine.Price,
+	// 	Stock:        medicine.Stock,
+	// 	Updated_at:   medicine.UpdatedAt,
+	// }, nil
+
+	return toMedicineDomain(medicine), nil
+}
+
+func (ms *medicineService) UpdateMedicine(ctx context.Context, medID uuid.UUID, med models.Medicine) (models.Medicine, error) {
+	medicine, err := ms.DB.UpdateMedicine(ctx, database.UpdateMedicineParams{
+		ID:           medID,
+		Name:         med.Name,
+		Description:  med.Description,
+		Manufacturer: med.Manufacturer,
+		Dosage:       med.Dosage,
+		Price:        med.Price,
+		Stock:        med.Stock,
+	})
+
+	if err != nil {
+		return models.Medicine{}, err
+	}
+
+	// return models.Medicine{
+	// 	ID:           medicine.ID,
+	// 	Name:         medicine.Name,
+	// 	Description:  medicine.Description,
+	// 	Manufacturer: medicine.Manufacturer,
+	// 	Dosage:       medicine.Dosage,
+	// 	Price:        medicine.Price,
+	// 	Stock:        medicine.Stock,
+	// 	Updated_at:   medicine.UpdatedAt,
+	// }, nil
+
+	return toMedicineDomain(medicine), nil
+
+}
+
+func toMedicineDomain(dbMed database.Medicine) models.Medicine {
+	return models.Medicine{
+		ID:           dbMed.ID,
+		Name:         dbMed.Name,
+		Description:  dbMed.Description,
+		Manufacturer: dbMed.Manufacturer,
+		Price:        dbMed.Price,
+		Stock:        dbMed.Stock,
+		Updated_at:   dbMed.UpdatedAt,
+		Created_at:   dbMed.CreatedAt,
+	}
+}
