1755e8b8c44346e43bec0127cca62036723e9dbe
Loknath Dhar <loknath-dhar@pm.me>
2025-01-06 13:26:59 +0600

    adding reset controller for dev env

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"log"
+	"medicine-app/internal/database"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +15,36 @@ import (
 // 	}
 // }
 
+var dev = "dev"
+
+type controller struct {
+	DB       *database.Queries
+	Platform string
+}
+
 func errorMsg(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+func NewController(db *database.Queries, platform string) *controller {
+	return &controller{
+		DB:       db,
+		Platform: platform,
+	}
+}
+
+func (ctrl *controller) HandlerReset(ctx *gin.Context) {
+	log.Println(ctrl.Platform)
+
+	if ctrl.Platform != dev {
+		ctx.Status(http.StatusForbidden)
+		return
+	}
+
+	if err := ctrl.DB.Reset(ctx); err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
+		return
+	}
+
+	ctx.Status(http.StatusNoContent)
 }
