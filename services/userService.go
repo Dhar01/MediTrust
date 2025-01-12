0ad7c19338123fa61e245de65370196a419d8aea
Loknath Dhar <loknath-dhar@pm.me>
2025-01-13 01:21:38 +0600

    fixing structure

--- services/userService.go
+++ services/userService.go
@@ -9,98 +9,39 @@ import (
 )
 
 type userService struct {
-	DB *database.Queries
+	Repo models.UserRepository
 }
 
-func NewUserService(db *database.Queries) models.UserService {
-	if db == nil {
-		panic("database can't be nil")
+func NewUserService(repo models.UserRepository) models.UserService {
+	if repo == nil {
+		panic("repo can't be nil")
 	}
 
 	return &userService{
-		DB: db,
+		Repo: repo,
 	}
 }
 
-func (us *userService) CreateUser(ctx context.Context, user models.User) (models.User, error) {
-	person, err := us.DB.CreateUser(ctx, database.CreateUserParams{
-		FirstName: user.Name.FirstName,
-		LastName:  user.Name.LastName,
-		Email:     user.Email,
-		Phone:     user.Phone,
-		Age:       user.Age,
-	})
-
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return toUserDomain(person), nil
+func (us *userService) CreateUser(ctx context.Context, user models.CreateUserDTO) (models.User, error) {
+	return us.Repo.Create(ctx, models.User{})
 }
 
 func (us *userService) FindUserByID(ctx context.Context, userID uuid.UUID) (models.User, error) {
-	user, err := us.DB.GetUserByID(ctx, userID)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return toUserDomain(user), nil
+	return us.Repo.FindByID(ctx, userID)
 }
 
 func (us *userService) FindUserByPhone(ctx context.Context, phone string) (models.User, error) {
-	user, err := us.DB.GetUserByPhone(ctx, phone)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return toUserDomain(user), nil
+	return us.Repo.FindByPhone(ctx, phone)
 }
 
 func (us *userService) FindUserByEmail(ctx context.Context, email string) (models.User, error) {
-	user, err := us.DB.GetUserByEmail(ctx, email)
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return toUserDomain(user), nil
+	return us.Repo.FindByEmail(ctx, email)
 }
 
-func (us *userService) UpdateUser(ctx context.Context, userID uuid.UUID, user models.User) (models.User, error) {
-	person, err := us.DB.UpdateUser(ctx, database.UpdateUserParams{
-		FirstName: user.Name.FirstName,
-		LastName: user.Name.LastName,
-		Email: user.Email,
-		Age: user.Age,
-		Phone: user.Phone,
-		ID: userID,
-	})
-
-	if err != nil {
-		return models.User{}, err
-	}
-
-	return toUserDomain(person), nil
+func (us *userService) UpdateUser(ctx context.Context, userID uuid.UUID, user models.UpdateUserDTO) (models.User, error) {
+	return us.Repo.Update(ctx, models.User{})
 }
 
 func (us *userService) DeleteUser(ctx context.Context, userID uuid.UUID) error {
-	if err := us.DB.DeleteUser(ctx, userID); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func toUserDomain(dbUser database.User) models.User {
-	return models.User{
-		ID: dbUser.ID,
-		Name: models.Name{
-			FirstName: dbUser.FirstName,
-			LastName:  dbUser.LastName,
-		},
-		Email:     dbUser.Email,
-		Age:       dbUser.Age,
-		Phone:     dbUser.Phone,
-		CreatedAt: dbUser.CreatedAt,
-		UpdatedAt: dbUser.UpdatedAt,
-	}
+	return us.Repo.Delete(ctx, userID)
 }
