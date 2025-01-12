d0b0a4f081060ca38e88db2e0c575d26db76d620
Loknath Dhar <loknath-dhar@pm.me>
2025-01-12 22:06:38 +0600

    update CreateMedicine and other structure

--- services/medicineService.go
+++ services/medicineService.go
@@ -2,102 +2,91 @@ package service
 
 import (
 	"context"
-	"medicine-app/internal/database"
+	"errors"
 	"medicine-app/models"
 
 	"github.com/google/uuid"
 )
 
+var (
+	errPriceNegative        = errors.New("medicine price can't be negative")
+	errStockNegative        = errors.New("medicine stock can't be negative")
+	errMedNameNotFound      = errors.New("medicine name not provided")
+	errDosageNotProvided    = errors.New("medicine dosage not provided")
+	errManufacturerNotFound = errors.New("medicine manufacturer not provided")
+)
+
 type medicineService struct {
-	DB *database.Queries
+	Repo models.MedicineRepository
 }
 
-func NewMedicineService(db *database.Queries) models.MedicineService {
-	if db == nil {
-		panic("database can't be nil")
+func NewMedicineService(repo models.MedicineRepository) models.MedicineService {
+	if repo == nil {
+		panic("repository can't be nil")
 	}
 
 	return &medicineService{
-		DB: db,
+		Repo: repo,
 	}
 }
 
-func (ms *medicineService) CreateMedicine(ctx context.Context, newMed models.Medicine) (models.Medicine, error) {
-	medicine, err := ms.DB.CreateMedicine(ctx, database.CreateMedicineParams{
+func (ms *medicineService) CreateMedicine(ctx context.Context, newMed models.CreateMedicineDTO) (models.Medicine, error) {
+	var emptyMed models.Medicine
+
+	if newMed.Name == "" {
+		return emptyMed, errMedNameNotFound
+	}
+
+	if newMed.Manufacturer == "" {
+		return emptyMed, errManufacturerNotFound
+	}
+
+	if newMed.Dosage == "" {
+		return emptyMed, errDosageNotProvided
+	}
+
+	if newMed.Price < 0 {
+		return emptyMed, errPriceNegative
+	}
+
+	if newMed.Stock < 0 {
+		return emptyMed, errStockNegative
+	}
+
+	medicine := models.Medicine{
 		Name:         newMed.Name,
 		Dosage:       newMed.Dosage,
 		Description:  newMed.Description,
 		Manufacturer: newMed.Manufacturer,
 		Price:        newMed.Price,
 		Stock:        newMed.Stock,
-	})
-
-	if err != nil {
-		return models.Medicine{}, err
 	}
 
-	return toMedicineDomain(medicine), nil
+	return ms.Repo.Create(ctx, medicine)
 }
 
 func (ms *medicineService) DeleteMedicine(ctx context.Context, medID uuid.UUID) error {
-	if err := ms.DB.DeleteMedicine(ctx, medID); err != nil {
-		return err
-	}
-
-	return nil
+	return ms.Repo.Delete(ctx, medID)
 }
 
 func (ms *medicineService) GetMedicines(ctx context.Context) ([]models.Medicine, error) {
-	var medList []models.Medicine
-
-	medicines, err := ms.DB.GetMedicines(ctx)
-	if err != nil {
-		return medList, err
-	}
-
-	for _, medicine := range medicines {
-		medList = append(medList, toMedicineDomain(medicine))
-	}
-
-	return medList, nil
+	return ms.Repo.FindAll(ctx)
 }
 
 func (ms *medicineService) GetMedicineByID(ctx context.Context, medID uuid.UUID) (models.Medicine, error) {
-	medicine, err := ms.DB.GetMedicine(ctx, medID)
-	if err != nil {
-		return models.Medicine{}, err
-	}
-
-	return toMedicineDomain(medicine), nil
+	return ms.Repo.FindByID(ctx, medID)
 }
 
-func (ms *medicineService) UpdateMedicine(ctx context.Context, medID uuid.UUID, med models.Medicine) (models.Medicine, error) {
-	medicine, err := ms.DB.UpdateMedicine(ctx, database.UpdateMedicineParams{
+func (ms *medicineService) UpdateMedicine(ctx context.Context, medID uuid.UUID, med models.UpdateMedicineDTO) (models.Medicine, error) {
+	medicine := models.Medicine{
 		ID:           medID,
-		Name:         med.Name,
-		Description:  med.Description,
-		Manufacturer: med.Manufacturer,
-		Dosage:       med.Dosage,
-		Price:        med.Price,
-		Stock:        med.Stock,
-	})
-
-	if err != nil {
-		return models.Medicine{}, err
+		Name:         *med.Name,
+		Description:  *med.Description,
+		Manufacturer: *med.Manufacturer,
+		Dosage:       *med.Dosage,
+		Price:        *med.Price,
+		Stock:        *med.Stock,
 	}
 
-	return toMedicineDomain(medicine), nil
-
-}
-
-func toMedicineDomain(dbMed database.Medicine) models.Medicine {
-	return models.Medicine{
-		ID:           dbMed.ID,
-		Name:         dbMed.Name,
-		Description:  dbMed.Description,
-		Manufacturer: dbMed.Manufacturer,
-		Price:        dbMed.Price,
-		Stock:        dbMed.Stock,
-		Updated_at:   dbMed.UpdatedAt,
-	}
+	return ms.Repo.Update(ctx, medicine)
 }
