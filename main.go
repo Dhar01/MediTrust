fc4e969fc22eca8647dc70f2fe1c7038856b9736
Loknath Dhar <loknath-dhar@pm.me>
2025-01-19 00:06:52 +0600

    decoupling main file

--- main.go
+++ main.go
@@ -2,59 +2,28 @@ package main
 
 import (
 	"log"
+	"medicine-app/api"
 	"medicine-app/config"
-	ctrl "medicine-app/controllers"
-	repo "medicine-app/repository"
-	service "medicine-app/services"
 
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	apiBase = "/api/v1"
-
-	medicineBase     = apiBase + "/medicines"
-	medicineBaseByID = medicineBase + "/:medID"
-
-	usersBase    = apiBase + "/users"
-	userBaseByID = usersBase + "/:userID"
-)
+const apiBase = "/api/v1"
 
 func main() {
 	cfg := config.NewConfig()
-
-	medRepo := repo.NewMedicineRepository(cfg.DB)
-	medService := service.NewMedicineService(medRepo)
-	medCtrl := ctrl.NewMedicineController(medService)
-
-	genRepo := repo.NewGeneralRepository(cfg.DB)
-	genService := service.NewGeneralService(genRepo)
-	genCtrl := ctrl.NewController(genService, cfg.Platform)
-
-	userRepo := repo.NewUserRepository(cfg.DB)
-	userService := service.NewUserService(userRepo)
-	userCtrl := ctrl.NewUserController(userService)
 
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
 
 	// medicines
-	router.GET(medicineBase, medCtrl.HandlerGetMedicines)
-	router.GET(medicineBaseByID, medCtrl.HandlerGetMedicineByID)
-	router.PUT(medicineBaseByID, medCtrl.HandlerUpdateMedicine)
-	router.POST(medicineBase, medCtrl.HandlerCreateMedicine)
-	router.DELETE(medicineBaseByID, medCtrl.HandlerDeleteMedicine)
+	api.MedicineRoutes(router.Group(apiBase), cfg)
 
 	// users
-	router.GET(userBaseByID, userCtrl.HandlerGetUserByID)
-	router.PUT(userBaseByID, userCtrl.HandlerUpdateUser)
-	router.POST(usersBase, userCtrl.HandlerCreateUser)
-	router.DELETE(userBaseByID, userCtrl.HandlerDeleteUser)
+	api.UserRoutes(router.Group(apiBase), cfg)
 
-	router.POST("/signup", userCtrl.HandlerSignUp)
-	router.POST("/login", userCtrl.HandlerLogIn)
-
-	router.POST("/reset", genCtrl.HandlerReset)
+	// general routes
+	api.GeneralRoutes(router.Group(apiBase), cfg)
 
 	port := ":8080"
 
