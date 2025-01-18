6e1d0f84730180e4c15427a3c5edb30d62d53892
Loknath Dhar <loknath-dhar@pm.me>
2025-01-19 00:06:20 +0600

    grouping users route

---
+++ api/users.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"medicine-app/config"
+	"medicine-app/controllers"
+	repo "medicine-app/repository"
+	service "medicine-app/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	usersBase    = "/users"
+	userBaseByID = usersBase + "/:userID"
+)
+
+func UserRoutes(router *gin.RouterGroup, cfg *config.Config) {
+	userRepo := repo.NewUserRepository(cfg.DB)
+	userService := service.NewUserService(userRepo)
+	userCtrl := controllers.NewUserController(userService)
+
+	// GET route for users
+	router.GET(userBaseByID, userCtrl.HandlerGetUserByID)
+
+	// PUT route for users
+	router.PUT(userBaseByID, userCtrl.HandlerUpdateUser)
+
+	// POST route for users
+	router.POST(usersBase, userCtrl.HandlerCreateUser)
+	router.POST("/signup", userCtrl.HandlerSignUp)
+	router.POST("/login", userCtrl.HandlerLogIn)
+
+	// DELETE route for users
+	router.DELETE(userBaseByID, userCtrl.HandlerDeleteUser)
+}
