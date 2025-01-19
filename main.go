e0ff356318b46c773923cf10b8d743a50bd1bf5d
Loknath Dhar <loknath-dhar@pm.me>
2025-01-19 12:51:25 +0600

    planning to use swagger for docs

--- main.go
+++ main.go
@@ -10,6 +10,20 @@ import (
 
 const apiBase = "/api/v1"
 
+// @title           Medicine-Shop Swagger API
+// @version         1.0
+// @description     Swagger API for Medicine Shop.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Loknath Dhar
+// @contact.email  loknathdhar01@yahoo.com
+
+// @license.name  MIT
+// @license.url
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
 func main() {
 	cfg := config.NewConfig()
 
@@ -22,12 +36,15 @@ func main() {
 	// users
 	api.UserRoutes(router.Group(apiBase), cfg)
 
+	// admin
+	api.AdminRoutes(router.Group(apiBase), cfg)
+
 	// general routes
 	api.GeneralRoutes(router.Group(apiBase), cfg)
 
 	port := ":8080"
 
 	if err := router.Run(port); err != nil {
-		log.Fatalf("cant' run in port %s: %v", port, err)
+		log.Fatalf("can't run in port %s: %v", port, err)
 	}
 }
