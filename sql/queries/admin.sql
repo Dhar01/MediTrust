75ceb3ff879c389f3b609222ad0bc52388c91a91
Loknath Dhar <loknath-dhar@pm.me>
2025-01-19 13:10:43 +0600

    fixing sql queries

--- sql/queries/admin.sql
+++ sql/queries/admin.sql
@@ -1,22 +1,2 @@
--- name: CreateAdmin :one
-INSERT INTO admin_roles (
-    user_id, role, created_at, updated_at
-) VALUES (
-    $1,
-    $2,
-    NOW(),
-    NOW()
-)
-RETURNING *;
-
--- name: GetAdminByUserID :one
-SELECT * FROM admin_roles
-WHERE user_id = $1;
-
--- name: GetAdminWithPermission :many
-SELECT
-    admin_roles.*, permissions.name
-FROM admin_roles
-JOIN admin_permissions ON admin_roles.user_id = admin_permissions.admin_id
-JOIN permissions ON permissions.permission_id = admin_permissions.permission_id
-WHERE admin_roles.user_id = $1;
+-- name: GetPermissionsName :one
+SELECT * FROM permissions;
