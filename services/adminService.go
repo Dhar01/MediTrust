8f2f6332020d002efd1d2fe498d6c94d4d03e1ea
Loknath Dhar <loknath-dhar@pm.me>
2025-01-23 19:24:38 +0600

    adding adminService strucuture

--- services/adminService.go
+++ services/adminService.go
@@ -1,14 +1,22 @@
 package service
 
-// type adminService struct {
-// 	Repo models.AdminRepository
-// }
+import (
+	"context"
+	"medicine-app/models"
 
-// func NewAdminService(repo models.AdminRepository) models.AdminService {
-// 	return &adminService{
-// 		Repo: repo,
-// 	}
-// }
+	"github.com/google/uuid"
+)
 
-// func (as *adminService) UpdatePermissions(ctx context.Context, id uuid.UUID, permissions []models.Permission) error {
-// }
+type adminService struct {
+	Repo models.AdminRepository
+}
+
+func NewAdminService(repo models.AdminRepository) models.AdminService {
+	return &adminService{
+		Repo: repo,
+	}
+}
+
+func (as *adminService) UpdatePermissions(ctx context.Context, id uuid.UUID, permissions []models.Permission) error {
+	return nil
+}
