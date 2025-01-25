62eb1d037d1297242d67727094e84d717029becf
Loknath Dhar <loknath-dhar@pm.me>
2025-01-25 15:26:19 +0600

    update database

--- sql/schema/005_refreshTokens.sql
+++ sql/schema/005_refreshTokens.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     refreshToken TEXT PRIMARY KEY,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     expires_at TIMESTAMP NOT NULL,
-    revoked_at TIMESTAMP NOT NULL,
+    revoked_at TIMESTAMP,
 
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
