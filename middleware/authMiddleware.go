c9ecc831fbafe7ba052a2ba3b3547db0e76334af
Loknath Dhar <loknath-dhar@pm.me>
2025-01-25 13:06:41 +0600

    adding IsLoggedIn middleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -23,6 +23,21 @@ func AdminAuth(secret string) gin.HandlerFunc {
 	}
 }
 
+func IsLoggedIn(secret string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, role, err := getAuth(ctx, secret)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("user_id", id)
+		ctx.Set("role", role)
+		ctx.Next()
+	}
+}
+
 func getAuth(ctx *gin.Context, secret string) (uuid.UUID, string, error) {
 	token, err := auth.GetBearerToken(ctx.Request.Header)
 	if err != nil {
@@ -35,7 +50,6 @@ func getAuth(ctx *gin.Context, secret string) (uuid.UUID, string, error) {
 	}
 
 	return id, role, nil
-
 }
 
 func wrapNilError(err error) (uuid.UUID, string, error) {
