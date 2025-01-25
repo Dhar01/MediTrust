bacb50d7cfb7905441084d84bd5b14f2f518a614
Loknath Dhar <loknath-dhar@pm.me>
2025-01-25 15:24:59 +0600

    fix general model

--- models/model.go
+++ models/model.go
@@ -1,16 +1,25 @@
 package models
 
-import "context"
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 type GeneralService interface {
 	ResetMedicineService(ctx context.Context) error
 	ResetUserService(ctx context.Context) error
 	ResetAddressService(ctx context.Context) error
-	GenerateAccessToken(ctx context.Context) (string, error)
+	GenerateToken(ctx context.Context, headers http.Header) (ResponseTokenDTO, error)
+	RevokeRefreshToken(ctx context.Context, headers http.Header) error
 }
 
 type GeneralRepository interface {
 	ResetMedicineRepo(ctx context.Context) error
 	ResetUserRepo(ctx context.Context) error
 	ResetAddressRepo(ctx context.Context) error
+	RevokeToken(ctx context.Context, token string) error
+	CreateRefreshToken(ctx context.Context, token string, id uuid.UUID) error
+	FindUserFromToken(ctx context.Context, token string) (User, error)
 }
