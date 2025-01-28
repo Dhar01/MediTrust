e9abf5de33ca6353c56faa034b5ee4fdec09eade
Loknath Dhar <loknath-dhar@pm.me>
2025-01-29 02:04:03 +0600

    adding general controll

---
+++ controllers/generalCtrl.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"log"
+	"medicine-app/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var dev = "dev"
+
+type controller struct {
+	GeneralService models.GeneralService
+	Platform       string
+}
+
+func errorMsg(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func NewController(service models.GeneralService, platform string) *controller {
+	return &controller{
+		GeneralService: service,
+		Platform:       platform,
+	}
+}
+
+func (ctrl *controller) HandlerReset(ctx *gin.Context) {
+	log.Println(ctrl.Platform)
+
+	if ctrl.Platform != dev {
+		ctx.Status(http.StatusForbidden)
+		return
+	}
+
+	if err := ctrl.GeneralService.ResetMedicineService(ctx); err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
+		return
+	}
+
+	log.Println("Medicine database reset successfully!")
+
+	if err := ctrl.GeneralService.ResetAddressService(ctx); err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
+		return
+	}
+
+	log.Println("Address database reset successfully!")
+
+	if err := ctrl.GeneralService.ResetUserService(ctx); err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorMsg(err))
+		return
+	}
+
+	log.Println("User database reset successfully!")
+
+	ctx.Status(http.StatusNoContent)
+}
+
+func (ctrl *controller) HandlerRefresh(ctx *gin.Context) {
+	var reqToken models.ReqToken
+
+	if err := ctx.ShouldBindJSON(&reqToken); err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusBadRequest, errorMsg(err))
+		return
+	}
+
+	token, err := ctrl.GeneralService.GenerateToken(ctx, reqToken.RefreshToken)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusUnauthorized, errorMsg(err))
+		return
+	}
+
+	ctx.SetCookie("refresh_token", token.RefreshToken, int(time.Hour*7*24), "/", models.DomainName, true, true)
+
+	ctx.JSON(http.StatusCreated, models.ServerResponse{
+		AccessToken: token.AccessToken,
+	})
+}
+
+func (ctrl *controller) HandlerRevoke(ctx *gin.Context) {
+	if err := ctrl.GeneralService.RevokeRefreshToken(ctx, ctx.Request.Header); err != nil {
+		ctx.Error(err)
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "can't revoke refresh token"})
+		return
+	}
+
+	ctx.Status(http.StatusOK)
+}
