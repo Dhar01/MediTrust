bb9ad19b54413568a2c0dcfe2dbf3ed5c5159d90
Loknath Dhar <loknath-dhar@pm.me>
2025-01-30 18:11:01 +0600

    making scripts flexible: up/down

--- scripts/db_migration.sh
+++ scripts/db_migration.sh
@@ -4,5 +4,13 @@ if [ -f .env ]; then
     source .env
 fi
 
+# two commands are available: up and down
+# before using down, think and plan it
+# in production, it's just no no
+# also, do think about migrate up only
+# 'down' command is available/applicable for the first-time development.
+echo -n "Enter command (up/down): "
+read cmd
+
 cd sql/schema
-goose postgres $DB_URL up
+goose postgres $DB_URL $cmd
