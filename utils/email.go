e0c70f293676d4dd651823045dd90735cd8997b5
Loknath Dhar <loknath-dhar@pm.me>
2025-01-31 14:45:44 +0600

    fixing data abstraction

--- utils/email.go
+++ utils/email.go
@@ -9,7 +9,7 @@ import (
 
 func SendVerificationEmail(userEmail, firstName, domain, token string, port int) error {
 	m := gomail.NewMessage()
-	m.SetHeader("From", models.BackendEmail)
+	m.SetHeader("From", models.EmailAdmin)
 	m.SetHeader("To", userEmail)
 	m.SetHeader("Subject", "User Verification Email")
 
@@ -18,7 +18,7 @@ func SendVerificationEmail(userEmail, firstName, domain, token string, port int)
 		<p>To verify your email, click here: <a href="http://%s:%d/api/v1/verify?token=%s">Verify Email</a></p>
 	`, firstName, domain, port, token))
 
-	d := gomail.NewDialer(models.SMTPServer, models.SMTPPort, models.GmailUser, models.GmailPass)
+	d := gomail.NewDialer(models.SMTPServer, models.SMTPPort, models.EmailAdmin, models.EmailAdminPass)
 
 	if err := d.DialAndSend(m); err != nil {
 		return err
