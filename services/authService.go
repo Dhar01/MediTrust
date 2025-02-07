b3996d41e36f85538977ae1464c7f87e44f5f792
Loknath Dhar <loknath-dhar@pm.me>
2025-02-07 13:40:55 +0600

    refactor & fix service code

---
+++ services/authService.go
@@ -0,0 +1,150 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"medicine-app/internal/auth"
+	"medicine-app/models"
+	"medicine-app/utils"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type authService struct {
+	AuthRepo         models.AuthRepository
+	UserRepo         models.UserRepository
+	VerificationRepo models.VerificationRepository
+	Secret           string
+}
+
+func NewAuthService(authRepo models.AuthRepository, userRepo models.UserRepository, verificationRepo models.VerificationRepository, secret string) models.Authservice {
+	if authRepo == nil || userRepo == nil || verificationRepo == nil {
+		panic("repo can't be nil")
+	}
+
+	return &authService{
+		AuthRepo:         authRepo,
+		UserRepo:         userRepo,
+		VerificationRepo: verificationRepo,
+		Secret:           secret,
+	}
+}
+
+func (as *authService) SignUpUser(ctx context.Context, user models.SignUpUser) (models.SignUpResponse, error) {
+	// check if the aser exists with the associated email
+	available, _ := as.UserRepo.FindUser(ctx, models.Email, user.Email)
+	if available.Exist {
+		return wrapSignUpError(errUserExist)
+	}
+
+	// * first aser will always be admin
+	count, err := as.UserRepo.CountAvailableUsers(ctx)
+	if err != nil {
+		return wrapSignUpError(err)
+	}
+	var role string
+	if count > 0 {
+		role = models.Customer
+	} else {
+		role = models.Admin
+	}
+
+	/*
+		TODO: above piece of code need to be replaced later.
+		* For the MVP, I wanted create the first aser as Admin.
+		TODO: I'm planning to create a cmd to create admin.
+	*/
+
+	// TODO: need to work on field validation of asers (below)
+
+	person, err := NewUser(user, role)
+	if err != nil {
+		return wrapSignUpError(err)
+	}
+
+	newUser, err := as.UserRepo.SignUp(ctx, person)
+	if err != nil {
+		return wrapSignUpError(err)
+	}
+
+	verifyToken, err := auth.GenerateVerificationToken(newUser.ID, newUser.Role, as.Secret)
+	if err != nil {
+		return wrapSignUpError(err)
+	}
+
+	// TODO: getting slow response for this.
+	sendEmail(newUser, verifyToken)
+
+	return models.SignUpResponse{
+		ID: newUser.ID,
+	}, nil
+}
+
+func sendEmail(user models.User, token string) {
+	if err := utils.SendVerificationEmail(user.Email, user.Name.FirstName, models.DomainName, token, models.DomainPort); err != nil {
+		log.Println(err)
+	}
+}
+
+func (as *authService) LogInUser(ctx context.Context, login models.LogIn) (models.TokenResponseDTO, error) {
+	user, err := as.UserRepo.FindUser(ctx, models.Email, login.Email)
+	if err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	ok, err := as.VerificationRepo.GetVerification(ctx, user.ID)
+	if err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	if !ok {
+		return wrapTokenResponseError(fmt.Errorf("not verified"))
+	}
+
+	if err = auth.CheckPasswordHash(login.Password, user.HashPassword); err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	role, err := as.VerificationRepo.GetUserRole(ctx, user.ID)
+	if err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	accessToken, err := auth.GenerateAccessToken(user.ID, role, as.Secret, time.Minute*15)
+	if err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	refreshToken, err := auth.GenerateRefreshToken()
+	if err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	if err = as.AuthRepo.CreateRefreshToken(ctx, refreshToken, user.ID); err != nil {
+		return wrapTokenResponseError(err)
+	}
+
+	return models.TokenResponseDTO{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
+
+func (as *authService) LogoutUser(ctx context.Context, id uuid.UUID) error {
+	return as.AuthRepo.Logout(ctx, id)
+}
+
+func (as *authService) SetVerifiedUser(ctx context.Context, token string) error {
+	id, err := auth.ValidateVerificationToken(token, as.Secret)
+	if err != nil {
+		return err
+	}
+
+	if err = as.VerificationRepo.SetVerification(ctx, id); err != nil {
+		return err
+	}
+
+	return nil
+}
--- services/userService.go
+++ services/userService.go
@@ -3,31 +3,27 @@ package service
 import (
 	"context"
 	"errors"
-	"fmt"
-	"log"
 	"medicine-app/internal/auth"
 	"medicine-app/models"
-	"medicine-app/utils"
-	"time"
 
 	"github.com/google/uuid"
 )
 
 var errUserExist = errors.New("user exist")
 
-type userService struct {
-	Repo   models.UserRepository
-	Secret string
+type userProfileService struct {
+	UserRepo models.UserRepository
+	Secret   string
 }
 
-func NewUserService(repo models.UserRepository, secret string) models.UserService {
-	if repo == nil {
+func NewUserProfileService(userRepo models.UserRepository, secret string) models.UserProfileService {
+	if userRepo == nil {
 		panic("repo can't be nil")
 	}
 
-	return &userService{
-		Repo:   repo,
-		Secret: secret,
+	return &userProfileService{
+		UserRepo: userRepo,
+		Secret:   secret,
 	}
 }
 
@@ -48,125 +44,8 @@ func NewUser(signUp models.SignUpUser, role string) (models.User, error) {
 	}, nil
 }
 
-func (us *userService) SignUpUser(ctx context.Context, user models.SignUpUser) (models.SignUpResponse, error) {
-	// check if the user exists with the associated email
-	available, _ := us.Repo.FindUser(ctx, models.Email, user.Email)
-	if available.Exist {
-		return wrapSignUpError(errUserExist)
-	}
-
-	// * first user will always be admin
-	count, err := us.Repo.CountAvailableUsers(ctx)
-	if err != nil {
-		return wrapSignUpError(err)
-	}
-	var role string
-	if count > 0 {
-		role = models.Customer
-	} else {
-		role = models.Admin
-	}
-
-	/*
-		TODO: above piece of code need to be replaced later.
-		* For the MVP, I wanted create the first user as Admin.
-		TODO: I'm planning to create a cmd to create admin.
-	*/
-
-	// TODO: need to work on field validation of users (below)
-
-	person, err := NewUser(user, role)
-	if err != nil {
-		return wrapSignUpError(err)
-	}
-
-	newUser, err := us.Repo.SignUp(ctx, person)
-	if err != nil {
-		return wrapSignUpError(err)
-	}
-
-	verifyToken, err := auth.GenerateVerificationToken(newUser.ID, newUser.Role, us.Secret)
-	if err != nil {
-		return wrapSignUpError(err)
-	}
-
-	// TODO: getting slow response for this.
-	sendEmail(newUser, verifyToken)
-
-	return models.SignUpResponse{
-		ID: newUser.ID,
-	}, nil
-}
-
-func sendEmail(user models.User, token string) {
-	if err := utils.SendVerificationEmail(user.Email, user.Name.FirstName, models.DomainName, token, models.DomainPort); err != nil {
-		log.Println(err)
-	}
-}
-
-func (us *userService) LogInUser(ctx context.Context, login models.LogIn) (models.TokenResponseDTO, error) {
-	user, err := us.Repo.FindUser(ctx, models.Email, login.Email)
-	if err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	ok, err := us.Repo.GetVerification(ctx, user.ID)
-	if err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	if !ok {
-		return wrapTokenResponseError(fmt.Errorf("not verified"))
-	}
-
-	if err = auth.CheckPasswordHash(login.Password, user.HashPassword); err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	role, err := us.Repo.GetUserRole(ctx, user.ID)
-	if err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	accessToken, err := auth.GenerateAccessToken(user.ID, role, us.Secret, time.Minute*15)
-	if err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	refreshToken, err := auth.GenerateRefreshToken()
-	if err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	if err = us.Repo.CreateRefreshToken(ctx, refreshToken, user.ID); err != nil {
-		return wrapTokenResponseError(err)
-	}
-
-	return models.TokenResponseDTO{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
-	}, nil
-}
-
-func (us *userService) SetVerifiedUser(ctx context.Context, token string) error {
-	id, err := auth.ValidateVerificationToken(token, us.Secret)
-	if err != nil {
-		return err
-	}
-
-	if err = us.Repo.SetVerification(ctx, id); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (us *userService) LogoutUser(ctx context.Context, id uuid.UUID) error {
-	return us.Repo.Logout(ctx, id)
-}
-
-func (us *userService) UpdateUser(ctx context.Context, userID uuid.UUID, user models.UpdateUserDTO) (models.UserResponseDTO, error) {
-	oldInfo, err := us.Repo.FindByID(ctx, userID)
+func (us *userProfileService) UpdateUser(ctx context.Context, userID uuid.UUID, user models.UpdateUserDTO) (models.UserResponseDTO, error) {
+	oldInfo, err := us.UserRepo.FindByID(ctx, userID)
 	// log.Printf("OLDINFO: %+v", oldInfo)
 	if err != nil {
 		return wrapUserError(err)
@@ -187,7 +66,7 @@ func (us *userService) UpdateUser(ctx context.Context, userID uuid.UUID, user mo
 
 	// log.Printf("UPDATEDUSER: %+v", person)
 
-	userUpdate, err := us.Repo.Update(ctx, person)
+	userUpdate, err := us.UserRepo.Update(ctx, person)
 	// log.Printf("Userupdate Data: %v", userUpdate)
 	if err != nil {
 		return wrapUserError(err)
@@ -196,12 +75,12 @@ func (us *userService) UpdateUser(ctx context.Context, userID uuid.UUID, user mo
 	return toUserDTODomain(userUpdate), nil
 }
 
-func (us *userService) DeleteUser(ctx context.Context, userID uuid.UUID) error {
-	return us.Repo.Delete(ctx, userID)
+func (us *userProfileService) DeleteUser(ctx context.Context, userID uuid.UUID) error {
+	return us.UserRepo.Delete(ctx, userID)
 }
 
-func (us *userService) FindUserByID(ctx context.Context, userID uuid.UUID) (models.UserResponseDTO, error) {
-	user, err := us.Repo.FindByID(ctx, userID)
+func (us *userProfileService) FindUserByID(ctx context.Context, userID uuid.UUID) (models.UserResponseDTO, error) {
+	user, err := us.UserRepo.FindByID(ctx, userID)
 	if err != nil {
 		return wrapUserError(err)
 	}
@@ -210,13 +89,13 @@ func (us *userService) FindUserByID(ctx context.Context, userID uuid.UUID) (mode
 }
 
 // FindUser by KEY. Key should be either Email or Phone.
-func (us *userService) FindUserByKey(ctx context.Context, key, value string) (models.UserResponseDTO, error) {
-	person, err := us.Repo.FindUser(ctx, key, value)
+func (us *userProfileService) FindUserByKey(ctx context.Context, key, value string) (models.UserResponseDTO, error) {
+	person, err := us.UserRepo.FindUser(ctx, key, value)
 	if err != nil {
 		return wrapUserError(err)
 	}
 
-	user, err := us.Repo.FindByID(ctx, person.ID)
+	user, err := us.UserRepo.FindByID(ctx, person.ID)
 	if err != nil {
 		return wrapUserError(err)
 	}
