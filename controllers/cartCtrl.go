3801346f77ee2cb02c3ff853e3a358dd24857c78
Loknath Dhar <loknath-dhar@pm.me>
2025-03-08 00:24:50 +0600

    adding HandlerGetCart controller

--- controllers/cartCtrl.go
+++ controllers/cartCtrl.go
@@ -25,4 +25,32 @@ func (cc *cartController) HandlerCreateCart(ctx *gin.Context) {
 		return
 	}
 
+	// * To be continue
+}
+
+// HandlerGetCart will fetch the data of a cart by UserID
+//
+//	@Summary		Fetch the data of a cart by userID
+//	@Description	Fetch the data of a cart using userID
+//	@Tags			cart
+//	@Accept			string
+//	@Produce		json
+//	@Success		200	{object}	db.Cart					"details of a cart"
+//	@Failure		401	{object}	dto.ErrorResponseDTO	"The user is not authorized"
+//	@Failure		500	{object}	dto.ErrorResponseDTO	'Internal	server	error"
+//	@Router			/cart [get]
+func (cc *cartController) HandlerGetCart(ctx *gin.Context) {
+	userID, ok := getUserID(ctx)
+	if !ok {
+		errorResponse(ctx, http.StatusUnauthorized, fmt.Errorf("UserID not found"))
+		return
+	}
+
+	cart, err := cc.cartService.GetCart(ctx.Request.Context(), userID)
+	if err != nil {
+		errorResponse(ctx, http.StatusInternalServerError, fmt.Errorf("cart details not found"))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, cart)
 }
