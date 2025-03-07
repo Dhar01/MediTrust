ab828ca23ce8a573232c39319e5defcb0ddce21f
Loknath Dhar <loknath-dhar@pm.me>
2025-03-08 00:15:09 +0600

    GetCartItems returns medicine name

--- sql/queries/carts.sql
+++ sql/queries/carts.sql
@@ -1,1 +1,13 @@
--- name: CreateCart :one
+-- name: GetCart :many
+SELECT
+    cart.id AS cart_id,
+    cart.created_at,
+    ci.id,
+    ci.medicine_id,
+    m.name AS medicine_name,
+    ci.quantity,
+    m.price AS price
+FROM cart
+LEFT JOIN cart_item ci ON cart.id = ci.cart_id
+LEFT JOIN medicines m ON ci.medicine_id = m.id
+WHERE cart.user_id = $1;
