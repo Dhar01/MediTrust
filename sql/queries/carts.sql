cce323e6a208b0dea5e9f458c915c6334826b710
Loknath Dhar <loknath-dhar@pm.me>
2025-03-09 00:38:57 +0600

    delete cart from the db

--- sql/queries/carts.sql
+++ sql/queries/carts.sql
@@ -11,3 +11,11 @@ FROM cart
 LEFT JOIN cart_item ci ON cart.id = ci.cart_id
 LEFT JOIN medicines m ON ci.medicine_id = m.id
 WHERE cart.user_id = $1;
+
+-- name: DeleteCart :exec
+DELETE FROM cart
+WHERE cart.user_id = $1;
+
+-- name: RemoveCartItem :exec
+DELETE FROM cart_item
+WHERE cart_item.cart_id = $1;
