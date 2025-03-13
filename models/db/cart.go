63d8674d1324c03d68e6ee40492c88b0b206a375
Loknath Dhar <loknath-dhar@pm.me>
2025-03-13 20:38:16 +0600

    adding dto and structure cart

--- models/db/cart.go
+++ models/db/cart.go
@@ -9,12 +9,11 @@ import (
 type Cart struct {
 	ID         uuid.UUID `json:"id" example:"550e8400-e29b-41d4-a716-446655440000" format:"uuid"` // Unique ID of the cart
 	UserID     uuid.UUID
-	Items      []CartItems
+	Items      []CartItem
 	Created_At time.Time `json:"created_at"`
 }
 
-type CartItems struct {
-	ID       uuid.UUID
+type CartItem struct {
 	Serial   int32
 	CartID   uuid.UUID
 	MedID    uuid.UUID
