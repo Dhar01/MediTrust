c7d336f9c0d8757b839879bd2fb978cf94ac562b
Loknath Dhar <loknath-dhar@pm.me>
2025-04-01 14:16:30 +0600

    fixing errors on new introducing

--- services/medicineService.go
+++ services/medicineService.go
@@ -3,9 +3,8 @@ package service
 import (
 	"context"
 	"errors"
+	med "medicine-app/internal/api/medicines_gen"
 	"medicine-app/internal/database"
-	"medicine-app/models/db"
-	"medicine-app/models/dto"
 
 	"github.com/google/uuid"
 )
@@ -17,11 +16,11 @@ type medicineService struct {
 // MedicineService defines the business logic interface for medicine management
 // @Description Interface for medicine-related business logic
 type MedicineService interface {
-	CreateMedicine(ctx context.Context, medicine dto.CreateMedicineDTO) (db.Medicine, error)
-	DeleteMedicine(ctx context.Context, medID uuid.UUID) error
-	UpdateMedicine(ctx context.Context, medID uuid.UUID, med dto.UpdateMedicineDTO) (db.Medicine, error)
-	GetMedicines(ctx context.Context) ([]db.Medicine, error)
-	GetMedicineByID(ctx context.Context, medID uuid.UUID) (db.Medicine, error)
+	CreateMedicine(ctx context.Context, medicine med.CreateMedicineDTO) (med.Medicine, error)
+	DeleteMedicine(ctx context.Context, medID med.MedicineID) error
+	UpdateMedicine(ctx context.Context, medID med.MedicineID, medicine med.UpdateMedicineDTO) (med.Medicine, error)
+	GetMedicines(ctx context.Context) ([]med.Medicine, error)
+	GetMedicineByID(ctx context.Context, medID med.MedicineID) (med.Medicine, error)
 }
 
 func NewMedicineService(db *database.Queries) MedicineService {
@@ -34,14 +33,14 @@ func NewMedicineService(db *database.Queries) MedicineService {
 	}
 }
 
-func (ms *medicineService) CreateMedicine(ctx context.Context, newMed dto.CreateMedicineDTO) (db.Medicine, error) {
+func (ms *medicineService) CreateMedicine(ctx context.Context, newMed med.CreateMedicineDTO) (med.Medicine, error) {
 	medicine, err := ms.DB.CreateMedicine(ctx, database.CreateMedicineParams{
 		Name:         newMed.Name,
 		Dosage:       newMed.Dosage,
 		Description:  newMed.Description,
 		Manufacturer: newMed.Manufacturer,
-		Price:        newMed.Price,
-		Stock:        newMed.Stock,
+		Price:        int32(newMed.Price),
+		Stock:        int32(newMed.Stock),
 	})
 
 	if err != nil {
@@ -51,7 +50,7 @@ func (ms *medicineService) CreateMedicine(ctx context.Context, newMed dto.Create
 	return toMedicineDomain(medicine), nil
 }
 
-func (ms *medicineService) DeleteMedicine(ctx context.Context, medID uuid.UUID) error {
+func (ms *medicineService) DeleteMedicine(ctx context.Context, medID med.MedicineID) error {
 	if err := ms.DB.DeleteMedicine(ctx, medID); err != nil {
 		return err
 	}
@@ -59,8 +58,8 @@ func (ms *medicineService) DeleteMedicine(ctx context.Context, medID uuid.UUID) 
 	return nil
 }
 
-func (ms *medicineService) GetMedicines(ctx context.Context) ([]db.Medicine, error) {
-	var medList []db.Medicine
+func (ms *medicineService) GetMedicines(ctx context.Context) ([]med.Medicine, error) {
+	var medList []med.Medicine
 
 	medicines, err := ms.DB.GetMedicines(ctx)
 	if err != nil {
@@ -74,7 +73,7 @@ func (ms *medicineService) GetMedicines(ctx context.Context) ([]db.Medicine, err
 	return medList, nil
 }
 
-func (ms *medicineService) GetMedicineByID(ctx context.Context, medID uuid.UUID) (db.Medicine, error) {
+func (ms *medicineService) GetMedicineByID(ctx context.Context, medID med.MedicineID) (med.Medicine, error) {
 	medicine, err := ms.DB.GetMedicine(ctx, medID)
 	if err != nil {
 		return wrapMedicineError(err)
@@ -83,44 +82,43 @@ func (ms *medicineService) GetMedicineByID(ctx context.Context, medID uuid.UUID)
 	return toMedicineDomain(medicine), nil
 }
 
-func (ms *medicineService) UpdateMedicine(ctx context.Context, medID uuid.UUID, med dto.UpdateMedicineDTO) (db.Medicine, error) {
-	var emptyMed db.Medicine
+func (ms *medicineService) UpdateMedicine(ctx context.Context, medID med.MedicineID, medicine med.UpdateMedicineDTO) (med.Medicine, error) {
+	var emptyMed med.Medicine
 
 	oldMed, err := ms.DB.GetMedicine(ctx, medID)
 	if err != nil {
 		return emptyMed, errors.New("cannot find medicine")
 	}
 
-	medicine, err := ms.DB.UpdateMedicine(ctx, database.UpdateMedicineParams{
+	newMed, err := ms.DB.UpdateMedicine(ctx, database.UpdateMedicineParams{
 		ID:           medID,
-		Name:         updateField(med.Name, oldMed.Name),
-		Description:  updateField(med.Description, oldMed.Description),
-		Manufacturer: updateField(med.Manufacturer, oldMed.Manufacturer),
-		Dosage:       updateField(med.Dosage, oldMed.Dosage),
-		Price:        *updateIntPointerField(med.Price, &oldMed.Price),
-		Stock:        *updateIntPointerField(med.Stock, &oldMed.Stock),
+		Name:         updateField(*medicine.Name, oldMed.Name),
+		Description:  updateField(*medicine.Description, oldMed.Description),
+		Manufacturer: updateField(*medicine.Manufacturer, oldMed.Manufacturer),
+		Dosage:       updateField(*medicine.Dosage, oldMed.Dosage),
+		Price:        *updateIntPointerField(medicine.Price, &oldMed.Price),
+		Stock:        *updateIntPointerField(medicine.Stock, &oldMed.Stock),
 	})
 
 	if err != nil {
 		return wrapMedicineError(err)
 	}
 
-	return toMedicineDomain(medicine), nil
+	return toMedicineDomain(newMed), nil
 }
 
-func wrapMedicineError(err error) (db.Medicine, error) {
-	return db.Medicine{}, err
+func wrapMedicineError(err error) (med.Medicine, error) {
+	return med.Medicine{}, err
 }
 
-func toMedicineDomain(dbMed database.Medicine) db.Medicine {
-	return db.Medicine{
-		ID:           dbMed.ID,
-		Name:         dbMed.Name,
-		Dosage:       dbMed.Dosage,
-		Description:  dbMed.Description,
-		Manufacturer: dbMed.Manufacturer,
-		Price:        dbMed.Price,
-		Stock:        dbMed.Stock,
-		Updated_at:   dbMed.UpdatedAt,
+func toMedicineDomain(dbMed database.Medicine) med.Medicine {
+	return med.Medicine{
+		Id:           (*uuid.UUID)(&dbMed.ID),
+		Name:         &dbMed.Name,
+		Dosage:       &dbMed.Dosage,
+		Description:  &dbMed.Description,
+		Manufacturer: &dbMed.Manufacturer,
+		Price:        &dbMed.Price,
+		Stock:        &dbMed.Stock,
 	}
 }
