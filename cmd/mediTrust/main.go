886e9b2cf05200bfed7fe9d2433089493ffd93ec
Loknath Dhar <loknath-dhar@pm.me>
2025-04-17 14:51:16 +0600

    wiring echo

--- cmd/mediTrust/main.go
+++ cmd/mediTrust/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"log"
-	"medicine-app/internal/handlers"
 	"medicine-app/config"
-	"medicine-app/models"
 	srv "medicine-app/internal/services"
+	"medicine-app/models"
+	"net/http"
 
 	_ "medicine-app/docs"
 
 	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 const apiBase = "/api/v1"
@@ -25,30 +27,43 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
-	router := gin.Default()
-	router.SetTrustedProxies(nil)
+	// router := gin.Default()
+	router := echo.New()
+	router.Logger.SetLevel(0)
+	router.Use(middleware.Logger())
+	// router.SetTrustedProxies(nil)
 
 	// medicines
-	srv.MedicineRoutes(router.Group(apiBase), cfg)
+	// srv.MedicineRoutes(router.Group(apiBase), cfg)
+	srv.MedicineRoutes(router, cfg, apiBase)
+
+	// authentication & authorization
+	srv.AuthRoutes(router, cfg, apiBase)
 
 	// users
-	handlers.UserRoutes(router.Group(apiBase), cfg)
+	srv.UserRoutes(router, cfg, apiBase)
+	// handlers.UserRoutes(router.Group(apiBase), cfg)
 
 	// admin
 	// api.AdminRoutes(router.Group(apiBase), cfg)
 
 	// general routes
-	handlers.GeneralRoutes(router.Group(apiBase), cfg)
+	// handlers.GeneralRoutes(router.Group(apiBase), cfg)
+	srv.GeneralRoutes(router, cfg, apiBase)
 
 	// documentation routes
-	handlers.DocumentationRoute(router.Group(apiBase))
+	// handlers.DocumentationRoute(router.Group(apiBase))
 
 	// cart routes
-	handlers.CartRoute(router.Group(apiBase), cfg)
+	// handlers.CartRoute(router.Group(apiBase), cfg)
 
 	port := ":" + cfg.Port
 
-	if err := router.Run(port); err != nil {
-		log.Fatalf("can't run in port %s: %v", cfg.Port, err)
+	// if err := router.Run(port); err != nil {
+	// 	log.Fatalf("can't run in port %s: %v", cfg.Port, err)
+	// }
+
+	if err := router.Start(port); err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
 }
