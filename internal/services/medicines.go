3d541c9b72435885f91f47205e87cb4d2089d8a0
Loknath Dhar <loknath-dhar@pm.me>
2025-04-18 19:47:15 +0600

    functional med service

--- internal/services/medicines.go
+++ internal/services/medicines.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"medicine-app/internal/repository"
 	"medicine-app/models"
 
@@ -10,7 +11,7 @@ import (
 
 type MedService interface {
 	CreateMedicine(ctx context.Context, med models.CreateMedicineDTO) (models.Medicine, error)
-	UpdateMedicine(ctx context.Context, med models.UpdateMedicineDTO) (models.Medicine, error)
+	UpdateMedicine(ctx context.Context, medID uuid.UUID, med models.UpdateMedicineDTO) (models.Medicine, error)
 	DeleteMedicine(ctx context.Context, medID uuid.UUID) error
 	FetchMedicineByID(ctx context.Context, medID uuid.UUID) (models.Medicine, error)
 }
@@ -26,17 +27,55 @@ func NewMedicineService(repo repository.MedicineRepository) MedService {
 }
 
 func (srv MedicineServiceImpl) CreateMedicine(ctx context.Context, med models.CreateMedicineDTO) (models.Medicine, error) {
-	return models.Medicine{}, nil
+	return srv.medicineRepo.CreateMedicine(ctx, med)
 }
 
 func (srv MedicineServiceImpl) DeleteMedicine(ctx context.Context, medID uuid.UUID) error {
-	return nil
+	return srv.medicineRepo.DeleteMedicine(ctx, medID)
 }
 
-func (srv MedicineServiceImpl) UpdateMedicine(ctx context.Context, med models.UpdateMedicineDTO) (models.Medicine, error) {
-	return models.Medicine{}, nil
+func (srv MedicineServiceImpl) UpdateMedicine(ctx context.Context, medID uuid.UUID, med models.UpdateMedicineDTO) (models.Medicine, error) {
+	oldMedicine, err := srv.medicineRepo.FetchMedicineByID(ctx, medID)
+	if err != nil {
+		return wrapMedicineErr(errors.New("medicine doesn't exist"))
+	}
+
+	newMedicine, err := srv.medicineRepo.UpdateMedicine(ctx, models.Medicine{
+		Name:         updateField(med.Name, oldMedicine.Name),
+		Dosage:       updateField(med.Dosage, oldMedicine.Dosage),
+		Manufacturer: updateField(med.Manufacturer, oldMedicine.Manufacturer),
+		Description:  updateField(med.Description, oldMedicine.Description),
+		Price:        *updateIntPointerField(&med.Price, &oldMedicine.Price),
+		Stock:        *updateIntPointerField(&med.Price, &oldMedicine.Price),
+	})
+
+	if err != nil {
+		return wrapMedicineErr(err)
+	}
+
+	return srv.medicineRepo.UpdateMedicine(ctx, newMedicine)
 }
 
 func (srv MedicineServiceImpl) FetchMedicineByID(ctx context.Context, medID uuid.UUID) (models.Medicine, error) {
-	return models.Medicine{}, nil
+	return srv.medicineRepo.FetchMedicineByID(ctx, medID)
+}
+
+func updateField(newValue, oldValue string) string {
+	if newValue == "" {
+		return oldValue
+	}
+
+	return newValue
+}
+
+func updateIntPointerField(newValue, oldValue *int32) *int32 {
+	if newValue == nil {
+		return oldValue
+	}
+
+	return newValue
+}
+
+func wrapMedicineErr(err error) (models.Medicine, error) {
+	return models.Medicine{}, err
 }
