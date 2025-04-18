8a00f2249e1768c28680588935acfb1494b7198e
Loknath Dhar <loknath-dhar@pm.me>
2025-04-18 18:23:35 +0600

    medService skeleton

---
+++ internal/services/services.go
@@ -0,0 +1,22 @@
+package services
+
+import "medicine-app/internal/repository"
+
+type Services struct {
+	MedService MedService
+	AuthService AuthService
+	UserService UserService
+}
+
+type AuthService any
+type UserService any
+
+func NewServices(repo *repository.Repository) *Services {
+	if repo == nil {
+		panic("repository can't be nil")
+	}
+
+	return &Services{
+		MedService: NewMedicineService(repo.MedRepo),
+	}
+}
