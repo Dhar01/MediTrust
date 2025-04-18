734613f533f5945fe7310813c8ebeba158e1ddb7
Loknath Dhar <loknath-dhar@pm.me>
2025-04-18 18:22:07 +0600

    re-adding models

---
+++ models/meddicine.go
@@ -0,0 +1,41 @@
+package models
+
+import "github.com/google/uuid"
+
+const CompanyName = ""
+
+var (
+	Admin    string = "admin"
+	Customer string = "customer"
+)
+
+// CreateMedicineDTO defines model for CreateMedicineDTO.
+type CreateMedicineDTO struct {
+	Description  string
+	Dosage       string
+	Manufacturer string
+	Name         string
+	Price        int32
+	Stock        int32
+}
+
+// Medicine defines model for Medicine.
+type Medicine struct {
+	Description  string
+	Dosage       string
+	Id           uuid.UUID
+	Manufacturer string
+	Name         string
+	Price        int32
+	Stock        int32
+}
+
+// UpdateMedicineDTO defines model for UpdateMedicineDTO.
+type UpdateMedicineDTO struct {
+	Description  string
+	Dosage       string
+	Manufacturer string
+	Name         string
+	Price        int32
+	Stock        int32
+}
