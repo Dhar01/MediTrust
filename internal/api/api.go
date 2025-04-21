29af9e20049a132fa9c2148b365c9be0cde1c70f
Loknath Dhar <loknath-dhar@pm.me>
2025-04-21 15:48:32 +0600

    testing middleware

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"log"
 	"medicine-app/config"
+	auth_gen "medicine-app/internal/api/authUser_gen"
+	general_gen "medicine-app/internal/api/gen_gen"
 	med_gen "medicine-app/internal/api/medicines_gen"
-	user_gen "medicine-app/internal/api/users_gen"
+	"medicine-app/internal/api/public_gen"
+	"medicine-app/internal/middleware"
 	"medicine-app/internal/repository"
 	"medicine-app/internal/services"
 
@@ -29,14 +33,39 @@ func MedicineRoutes(router *echo.Echo, cfg *config.Config, baseURL string) {
 	repo := repository.NewMedicineRepo(&cfg.DB.Medicine)
 	srv := services.NewMedicineService(repo)
 	api := newMedicineAPI(srv)
-	server := med_gen.NewStrictHandler(api, nil)
+
+	middle := middleware.NewMiddleware(cfg)
+
+	server := med_gen.NewStrictHandler(api, []med_gen.StrictMiddlewareFunc{
+		middle.RequireLoggedIn,
+	})
+
 	med_gen.RegisterHandlersWithBaseURL(router, server, baseURL)
 }
 
-func UserRoutes(router *echo.Echo, cfg *config.Config, baseURL string) {
-	repo := repository.NewUserRepo(&cfg.DB.User)
-	srv := services.NewUserService(repo)
-	api := newUserAPI(srv)
-	server := user_gen.NewStrictHandler(api, nil)
-	user_gen.RegisterHandlersWithBaseURL(router, server, baseURL)
+func PublicRoutes(router *echo.Echo, cfg *config.Config, baseURL string) {
+	repo := repository.NewPublicRepo(&cfg.DB.User)
+	srv := services.NewPublicService(repo, cfg)
+	api := newPublicAPI(srv)
+	server := public_gen.NewStrictHandler(api, nil)
+	public_gen.RegisterHandlersWithBaseURL(router, server, baseURL)
+}
+
+func AuthUserRoutes(router *echo.Echo, cfg *config.Config, baseURL string) {
+	authRepo := repository.NewAuthUserRepo(&cfg.DB.User)
+	publicRepo := repository.NewPublicRepo(&cfg.DB.User)
+	authSrv := services.NewAuthUserService(authRepo, publicRepo, cfg)
+	pubSrv := services.NewPublicService(publicRepo, cfg)
+	api := newAuthUserAPI(authSrv, pubSrv)
+	server := auth_gen.NewStrictHandler(api, nil)
+	auth_gen.RegisterHandlersWithBaseURL(router, server, baseURL)
+}
+
+func GeneralRoutes(router *echo.Echo, cfg *config.Config, baseURL string) {
+	helpRepo := repository.NewHelperRepo(&cfg.DB.Helper)
+	log.Println(cfg.Platform)
+	helpSrv := services.NewHelperService(helpRepo, cfg.Platform)
+	api := newHelperAPI(helpSrv)
+	server := general_gen.NewStrictHandler(api, nil)
+	general_gen.RegisterHandlersWithBaseURL(router, server, baseURL)
 }
