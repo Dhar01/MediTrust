a664a74e0b09bd523c25594fd88f2fd173dbe76c
Loknath Dhar <loknath-dhar@pm.me>
2025-04-26 01:46:43 +0600

    adding errors

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -5,14 +5,18 @@ import "errors"
 var (
 	ErrNotFound = errors.New("not found")
 
-	ErrMedicineNotExist  = errors.New("medicine not exist")
-	ErrMedicineExist     = errors.New("medicine with same ID/Name exists")
-	ErrMedicineNotUpdate = errors.New("medicine can't be updated")
+	ErrMedicineNotExist = errors.New("medicine not exist")
+	ErrMedicineExist    = errors.New("medicine with same ID/Name exists")
+	ErrMedicineUpdate   = errors.New("medicine can't be updated")
 
 	ErrUserNotExist       = errors.New("user not exist")
 	ErrUserNotAuthorized  = errors.New("user not authorized")
 	ErrUserInactive       = errors.New("user not active")
+	ErrUserRoleNotFound   = errors.New("user role not found")
+	ErrUserIdNotFound     = errors.New("user ID not found")
 	ErrEmailAlreadyExists = errors.New("email already being used")
 
 	ErrInternalServer = errors.New("internal server error")
+
+	ErrUserNotAdmin = errors.New("role is not admin")
 )
