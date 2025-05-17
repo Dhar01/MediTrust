c28faf1a91510da874c05d245917225c80b2eb32
Loknath Dhar <loknath-dhar@pm.me>
2025-05-18 02:15:42 +0600

    impl: connectDB and GetDB

---
+++ internal/database/dbConnect.go
@@ -0,0 +1,55 @@
+package database
+
+import (
+	"context"
+	"medicine-app/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func GetDB(rDbms config.RDBMS) (*Queries, error) {
+	dsn := getDSN(rDbms)
+
+	conn, err := connectDB(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return New(conn), nil
+}
+
+func connectDB(dsn string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = pool.Ping(context.Background()); err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func getDSN(rDbms config.RDBMS) string {
+	host := "host=" + rDbms.Env.Host
+	port := " port=" + rDbms.Env.Port
+	username := " user=" + rDbms.Access.User
+	database := " dbname=" + rDbms.Access.DbName
+	password := " password=" + rDbms.Access.Pass
+	timezone := " TimeZone=" + rDbms.Env.TimeZone
+
+	dsn := host + port + username + database + password + timezone
+
+	// ! currently working for the development
+	if rDbms.Ssl.SslMode == "" {
+		rDbms.Ssl.SslMode = "disable"
+	}
+
+	sslMode := " sslmode=" + rDbms.Ssl.SslMode
+
+	dsn += sslMode
+
+	return dsn
+}
--- internal/middleware/adminMiddleware.go
+++ internal/middleware/adminMiddleware.go
@@ -1,126 +1,104 @@
 package middleware
 
 import (
-	"medicine-app/config"
-	"medicine-app/internal/auth"
-	"medicine-app/internal/errs"
-
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 type HandlerFunc func(ctx echo.Context, request any) (response any, err error)
 
-type middleware struct {
-	cfg *config.Config
-}
+// type middleware struct {
+// 	cfg *config.Config
+// }
+// func NewMiddleware(cfg *config.Config) *middleware {
+// 	if cfg == nil {
+// 		panic("configuration can't be empty/nil")
+// 	}
+// 	return &middleware{
+// 		cfg: cfg,
+// 	}
+// }
 
-func NewMiddleware(cfg *config.Config) *middleware {
-	if cfg == nil {
-		panic("configuration can't be empty/nil")
-	}
+// func (m *middleware) IsUser(next HandlerFunc, operationID string) HandlerFunc {
+// 	return func(ctx echo.Context, request any) (any, error) {
+// 		if err := m.requireLoggedIn(ctx); err != nil {
+// 			return wrapErr(err)
+// 		}
+// 		role, ok := ctx.Get("role").(string)
+// 		if !ok {
+// 			return wrapErr(errs.ErrUserRoleNotFound)
+// 		}
+// 		if role != "customer" {
+// 			return wrapErr(errs.ErrUserNotExist)
+// 		}
+// 		return next(ctx, request)
+// 	}
+// }
 
-	return &middleware{
-		cfg: cfg,
-	}
-}
+// func (m *middleware) IsAdmin(next HandlerFunc, operationID string) HandlerFunc {
+// 	// admin protected
+// 	protected := map[string]bool{
+// 		"CreateNewMedicine":      true,
+// 		"DeleteMedicineByID":     true,
+// 		"UpdateMedicineInfoByID": true,
+// 	}
+// 	if needsAdmin, ok := protected[operationID]; !ok || !needsAdmin {
+// 		return next
+// 	}
+// 	return func(ctx echo.Context, request any) (any, error) {
+// 		if err := m.requireLoggedIn(ctx); err != nil {
+// 			return wrapErr(err)
+// 		}
+// 		role, ok := ctx.Get("role").(string)
+// 		if !ok {
+// 			return wrapErr(errs.ErrUserRoleNotFound)
+// 		}
+// 		if role != "admin" {
+// 			return wrapErr(errs.ErrUserNotAdmin)
+// 		}
+// 		return next(ctx, request)
+// 	}
+// }
 
-func (m *middleware) IsUser(next HandlerFunc, operationID string) HandlerFunc {
-	return func(ctx echo.Context, request any) (any, error) {
-		if err := m.requireLoggedIn(ctx); err != nil {
-			return wrapErr(err)
-		}
+// func (m *middleware) requireLoggedIn(ctx echo.Context) error {
+// 	token, err := getToken(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	id, role, err := m.getUserAuth(token)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if role == "" {
+// 		return errs.ErrUserRoleNotFound
+// 	}
+// 	if id == uuid.Nil {
+// 		return errs.ErrUserIdNotFound
+// 	}
+// 	ctx.Set("user_id", id)
+// 	ctx.Set("role", role)
+// 	return nil
+// }
 
-		role, ok := ctx.Get("role").(string)
-		if !ok {
-			return wrapErr(errs.ErrUserRoleNotFound)
-		}
+// func (m *middleware) getUserAuth(token string) (uuid.UUID, string, error) {
+// 	id, role, err := auth.ValidateAccessToken(token, m.cfg.SecretKey)
+// 	if err != nil {
+// 		return wrapNilError(err)
+// 	}
+// 	return id, role, nil
+// }
 
-		if role != "customer" {
-			return wrapErr(errs.ErrUserNotExist)
-		}
+// func getToken(ctx echo.Context) (string, error) {
+// 	token, err := auth.GetBearerToken(ctx.Request().Header)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return token, nil
+// }
 
-		return next(ctx, request)
-	}
-}
+// func wrapNilError(err error) (uuid.UUID, string, error) {
+// 	return uuid.Nil, "", err
+// }
 
-func (m *middleware) IsAdmin(next HandlerFunc, operationID string) HandlerFunc {
-	// admin protected
-	protected := map[string]bool{
-		"CreateNewMedicine":      true,
-		"DeleteMedicineByID":     true,
-		"UpdateMedicineInfoByID": true,
-	}
-
-	if needsAdmin, ok := protected[operationID]; !ok || !needsAdmin {
-		return next
-	}
-
-	return func(ctx echo.Context, request any) (any, error) {
-		if err := m.requireLoggedIn(ctx); err != nil {
-			return wrapErr(err)
-		}
-
-		role, ok := ctx.Get("role").(string)
-		if !ok {
-			return wrapErr(errs.ErrUserRoleNotFound)
-		}
-
-		if role != "admin" {
-			return wrapErr(errs.ErrUserNotAdmin)
-		}
-
-		return next(ctx, request)
-	}
-}
-
-func (m *middleware) requireLoggedIn(ctx echo.Context) error {
-	token, err := getToken(ctx)
-	if err != nil {
-		return err
-	}
-
-	id, role, err := m.getUserAuth(token)
-	if err != nil {
-		return err
-	}
-
-	if role == "" {
-		return errs.ErrUserRoleNotFound
-	}
-
-	if id == uuid.Nil {
-		return errs.ErrUserIdNotFound
-	}
-
-	ctx.Set("user_id", id)
-	ctx.Set("role", role)
-
-	return nil
-}
-
-func (m *middleware) getUserAuth(token string) (uuid.UUID, string, error) {
-	id, role, err := auth.ValidateAccessToken(token, m.cfg.SecretKey)
-	if err != nil {
-		return wrapNilError(err)
-	}
-
-	return id, role, nil
-}
-
-func getToken(ctx echo.Context) (string, error) {
-	token, err := auth.GetBearerToken(ctx.Request().Header)
-	if err != nil {
-		return "", err
-	}
-
-	return token, nil
-}
-
-func wrapNilError(err error) (uuid.UUID, string, error) {
-	return uuid.Nil, "", err
-}
-
-func wrapErr(err error) (any, error) {
-	return nil, err
-}
+// func wrapErr(err error) (any, error) {
+// 	return nil, err
+// }
