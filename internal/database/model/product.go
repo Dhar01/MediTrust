01819a73cab8362b256b4e684447006003dbc65a
Loknath Dhar <loknath-dhar@pm.me>
2025-06-07 17:54:52 +0600

    add: product model

--- internal/database/model/product.go
+++ internal/database/model/product.go
@@ -1,39 +1,64 @@
 package model
 
 import (
-	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
 )
 
-// ProductType defines the type for products available on the store
+// ProductType defines the type for products entity on the store
 type ProductType string
 
+// Constants for product types
 const (
 	ProductMedicine ProductType = "medicine"
 	ProductMedical  ProductType = "medical_instrument"
 )
 
-// Product defines the product entity for the store
+// Product defines the product entity for the store.
 type Product struct {
-	ID           uuid.UUID
-	Name         string
-	Manufacturer string
-	Description  string
-	Price        int32
-	Cost         int32
-	Stock        int32
-	Type         ProductType
-	Metadata     any
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           uuid.UUID   `json:"id"`
+	Name         string      `json:"name" validate:"required"`
+	Manufacturer string      `json:"manufacturer" validate:"required"`
+	Description  string      `json:"description"`
+	Price        int32       `json:"price" validate:"required"`
+	Cost         int32       `json:"cost" validate:"required"`
+	Stock        int32       `json:"stock" validate:"required"`
+	Type         ProductType `json:"type" validate:"required"`
+	CreatedAt    time.Time   `json:"created_at"`
+	UpdatedAt    time.Time   `json:"updated_at"`
 }
 
-// MedicineMetadata defines the "medicine" type of product entity
-type MedicineMetadata struct {
+// Medicine struct defines the structure of medicine type of product entity.
+type Medicine struct {
+	Product
 	Dosage     string    `json:"dosage"`
 	ExpiryDate time.Time `json:"expiry_date"`
+}
+
+// MedicineResponse structures the response for general users
+type MedicineResponse struct {
+	ID           uuid.UUID   `json:"id"`
+	Name         string      `json:"name"`
+	Manufacturer string      `json:"manufacturer"`
+	Description  string      `json:"description"`
+	Price        int32       `json:"price"`
+	Type         ProductType `json:"type"`
+	Dosage       string      `json:"dosage"`
+	ExpiryDate   time.Time   `json:"expiry_date"`
+}
+
+// GenericResponse method build the response for general users
+func (m *Medicine) GenericResponse() MedicineResponse {
+	return MedicineResponse{
+		ID:           m.ID,
+		Name:         m.Name,
+		Manufacturer: m.Manufacturer,
+		Description:  m.Description,
+		Price:        m.Price,
+		Dosage:       m.Dosage,
+		ExpiryDate:   m.ExpiryDate,
+	}
 }
 
 // IsTypeMedicine returns true if the product type is medicine
@@ -44,29 +69,4 @@ func (p *Product) IsTypeMedicine() bool {
 // IsTypeMedicalInstrument returns true if the product type is medicine_instrument
 func (p *Product) IsTypeMedicalInstrument() bool {
 	return p.Type == ProductMedical
-}
-
-// ProductRequest defines the request structure for product entity
-type ProductRequest struct {
-	Name         string          `json:"name" validate:"required"`
-	Manufacturer string          `json:"manufacturer" validate:"required"`
-	Description  string          `json:"description"`
-	Price        int32           `json:"price" validate:"required"`
-	Cost         int32           `json:"cost" validate:"required"`
-	Stock        int32           `json:"stock" validate:"required"`
-	Type         ProductType     `json:"type" validate:"required"`
-	Metadata     json.RawMessage `json:"metadata" validate:"required"` // raw JSON payload
-}
-
-// ProductResponse defines the response structure for product entity
-type ProductResponse struct {
-	ID           uuid.UUID   `json:"id"`
-	Name         string      `json:"name"`
-	Manufacturer string      `json:"manufacturer"`
-	Description  string      `json:"description"`
-	Price        int32       `json:"price"`
-	Type         ProductType `json:"type"`
-	Metadata     any         `json:"metadata"` // will be decoded before sending
-	CreatedAt    time.Time   `json:"createdAt"`
-	UpdatedAt    time.Time   `json:"updatedAt"`
 }
