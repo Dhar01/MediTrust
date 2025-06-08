60fb7e385aa7c1bf7d63b9fab9a9f3814e68730f
Loknath Dhar <loknath-dhar@pm.me>
2025-06-08 15:24:23 +0600

    add: repo model for medicine

---
+++ internal/repository/medicine.go
@@ -0,0 +1,83 @@
+package repository
+
+import (
+	"context"
+	"medicine-app/internal/database"
+	"medicine-app/internal/database/model"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+// MedicineRepo defines the interaction methods with the database
+type MedicineRepo interface {
+	Create(ctx context.Context, med model.Medicine) (*model.Medicine, error)
+	Update(ctx context.Context, med model.Medicine) (*model.Medicine, error)
+	FetchByID(ctx context.Context, medID uuid.UUID) (*model.Medicine, error)
+}
+
+var _ MedicineRepo = (*medPostgresRepo)(nil)
+
+// medPostgresRepo defines the db connection for medicine on postgres
+type medPostgresRepo struct {
+	db *database.Queries
+}
+
+// NewMedPostgresRepo will return a new Postgres connection for medicine domain
+func NewMedPostgresRepo(db *database.Queries) *medPostgresRepo {
+	return &medPostgresRepo{
+		db: db,
+	}
+}
+
+// Create creates a new medicine info on the database
+func (r *medPostgresRepo) Create(ctx context.Context, med model.Medicine) (*model.Medicine, error) {
+	newMedicine, err := r.db.CreateMedicine(ctx, database.CreateMedicineParams{
+		ID:     med.ID,
+		Dosage: med.Dosage,
+		ExpiryDate: pgtype.Timestamp{
+			Time: med.ExpiryDate,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return toMedicine(newMedicine), nil
+}
+
+// Update updates a medicine info on the database
+func (r *medPostgresRepo) Update(ctx context.Context, med model.Medicine) (*model.Medicine, error) {
+	updatedMed, err := r.db.UpdateMedicine(ctx, database.UpdateMedicineParams{
+		ID:     med.ID,
+		Dosage: med.Dosage,
+		ExpiryDate: pgtype.Timestamp{
+			Time: med.ExpiryDate,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return toMedicine(updatedMed), nil
+}
+
+// FetchByID returns a medicine info by its ID from the database
+func (r *medPostgresRepo) FetchByID(ctx context.Context, medID uuid.UUID) (*model.Medicine, error) {
+	medicine, err := r.db.GetMedicineInfoByID(ctx, medID)
+	if err != nil {
+		return nil, err
+	}
+
+	return toMedicine(medicine), nil
+}
+
+// helper: toMedicine converts database.Medicine type to model.Medicine type
+func toMedicine(medicine database.Medicine) *model.Medicine {
+	return &model.Medicine{
+		Dosage:     medicine.Dosage,
+		ExpiryDate: medicine.ExpiryDate.Time,
+	}
+}
