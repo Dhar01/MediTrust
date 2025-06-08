f98425d894b6248e4e69bb81d2d899f3aff6161c
Loknath Dhar <loknath-dhar@pm.me>
2025-06-08 19:36:44 +0600

    refactor: decoupled the products

--- internal/database/model/product.go
+++ internal/database/model/product.go
@@ -6,6 +6,11 @@ import (
 	"github.com/google/uuid"
 )
 
+type Response interface {
+	GenericResponse() any
+	AdminResponse() any
+}
+
 // ProductType defines the type for products entity on the store
 type ProductType string
 
@@ -17,48 +22,16 @@ const (
 
 // Product defines the product entity for the store.
 type Product struct {
-	ID           uuid.UUID   `json:"id"`
-	Name         string      `json:"name" validate:"required"`
-	Manufacturer string      `json:"manufacturer" validate:"required"`
-	Description  string      `json:"description"`
-	Price        int32       `json:"price" validate:"required"`
-	Cost         int32       `json:"cost" validate:"required"`
-	Stock        int32       `json:"stock" validate:"required"`
-	Type         ProductType `json:"type" validate:"required"`
-	CreatedAt    time.Time   `json:"created_at"`
-	UpdatedAt    time.Time   `json:"updated_at"`
-}
-
-// Medicine struct defines the structure of medicine type of product entity.
-type Medicine struct {
-	Product
-	Dosage     string    `json:"dosage"`
-	ExpiryDate time.Time `json:"expiry_date"`
-}
-
-// MedicineResponse structures the response for general users
-type MedicineResponse struct {
-	ID           uuid.UUID   `json:"id"`
-	Name         string      `json:"name"`
-	Manufacturer string      `json:"manufacturer"`
-	Description  string      `json:"description"`
-	Price        int32       `json:"price"`
-	Type         ProductType `json:"type"`
-	Dosage       string      `json:"dosage"`
-	ExpiryDate   time.Time   `json:"expiry_date"`
-}
-
-// GenericResponse method build the response for general users
-func (m *Medicine) GenericResponse() MedicineResponse {
-	return MedicineResponse{
-		ID:           m.ID,
-		Name:         m.Name,
-		Manufacturer: m.Manufacturer,
-		Description:  m.Description,
-		Price:        m.Price,
-		Dosage:       m.Dosage,
-		ExpiryDate:   m.ExpiryDate,
-	}
+	ID           uuid.UUID
+	Name         string
+	Manufacturer string
+	Description  string
+	Price        int32
+	Cost         int32
+	Stock        int32
+	Type         ProductType
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
 }
 
 // IsTypeMedicine returns true if the product type is medicine
