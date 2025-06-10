489d04782220358b69c6f8d43084031539d219e1
Loknath Dhar <loknath-dhar@pm.me>
2025-06-10 18:58:20 +0600

    routing updated

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,41 @@
 package router
 
 import (
+	"log"
 	"medicine-app/config"
+	"medicine-app/internal/database"
 	"medicine-app/internal/handler"
+	"medicine-app/internal/service"
+	"medicine-app/internal/store"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 )
 
 func SetUpRouter(conf *config.Configuration, e *echo.Echo) {
+	dsn := database.GetDSN(conf.Database.RDbms)
+
+	db, err := database.GetDB(conf.Database.RDbms)
+	if err != nil {
+		log.Fatalf("can't get to DB instance: %v", err)
+	}
+
+	dbPool, err := database.ConnectDB(dsn)
+	if err != nil {
+		log.Fatalf("can't connect to DB: %v", err)
+	}
+
+	store := store.NewStore(db)
+
+	v := validator.New()
+
 	e.GET("/", handler.APIStatus)
+
+	// Product route - medicine
+	medSrv := service.NewMedService(dbPool, store)
+	medHandler := handler.NewMedHandler(v, medSrv)
+
+	e.POST("/products/medicines", medHandler.CreateMedicine)
+	e.PUT("/products/medicines/:productID", medHandler.UpdateMedicine)
+	e.DELETE("/products/medicines/:productID", medHandler.DeleteMedicine)
 }
