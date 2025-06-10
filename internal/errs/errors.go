9805df6591b9446d4cf86bb93e5055ffae38be4c
Loknath Dhar <loknath-dhar@pm.me>
2025-06-10 18:58:00 +0600

    erros updated

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -3,20 +3,9 @@ package errs
 import "errors"
 
 var (
-	ErrNotFound = errors.New("resource not found")
-
-	ErrMedicineNotExist = errors.New("medicine not exist")
-	ErrMedicineExist    = errors.New("medicine with same ID/Name exists")
-	ErrMedicineUpdate   = errors.New("medicine can't be updated")
-
-	ErrUserNotExist       = errors.New("user not exist")
-	ErrUserNotAuthorized  = errors.New("user not authorized")
-	ErrUserInactive       = errors.New("user not active")
-	ErrUserRoleNotFound   = errors.New("user role not found")
-	ErrUserIdNotFound     = errors.New("user ID not found")
-	ErrEmailAlreadyExists = errors.New("email already being used")
-
-	ErrInternalServer = errors.New("internal server error")
-
-	ErrUserNotAdmin = errors.New("role is not admin")
+	ErrNotFound     = errors.New("resource not found")
+	ErrConflict     = errors.New("resource already exists")
+	ErrInvalidInput = errors.New("invalid input")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrForbidden    = errors.New("forbidden")
 )
